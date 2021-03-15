07a1dac8e5fdc6dcad97327b9b74a8401ecbae00
Anibal Portillo <48638366+AnibalLP@users.noreply.github.com>
2021-03-14 23:32:18 -0600

    hasta aqui pudimos llegar

--- matriculas.php
+++ matriculas.php
@@ -1,1 +1,14 @@
-<h1><a href="index.php">Muchos nos esforzamos cada dia, para aprender de una mejor manera!</a></h1>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/estilos.css" rel="stylesheet">
+        
+    </head>
+    <body>
+    <h1><a href="index.php">Muchos nos esforzamos cada dia, para aprender de una mejor manera!</a></h1>
+    
+    </body>
+</html>
